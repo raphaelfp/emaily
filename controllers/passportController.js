615efcd29b2df4d317b650223bbb88997c10064c
Raphael Fernandes <raphaelfp.eng@gmail.com>
2017-07-31 02:17:47 -0300

    added ssl redirect

--- controllers/passportController.js
+++ controllers/passportController.js
@@ -1,11 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const cookieSession = require('cookie-session');
+var sslRedirect = require('heroku-ssl-redirect');
+
 const keys = require('../config/keys');
 
 require('../services/passportService');
 
 module.exports = app => {
+	app.use(sslRedirect());
 	app.use(
 		cookieSession({
 			maxAge: 30 * 24 * 60 * 60 * 1000,

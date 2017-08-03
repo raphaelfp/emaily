1e22d1fdfba6eb3db08a85c9bf665f648960c118
Raphael Fernandes <raphaelfp.eng@gmail.com>
2017-08-02 22:27:56 -0300

    fix heroku-postbuild

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 require('./server')(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (true) {
 	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
